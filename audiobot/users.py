ce511f84a7d224df1db964bc0e82f7cbc4c59bb2
Joao Meyer <1994meyer@gmail.com>
2020-05-10 00:12:29 -0300

    Created users interface with firestore

---
+++ audiobot/users.py
@@ -0,0 +1,27 @@
+import logging
+
+from google.cloud import firestore
+
+
+class Users(object):
+    logger = logging.getLogger('Users')
+
+    def __init__(self):
+        super(Users, self).__init__()
+        self.client = firestore.Client()
+
+    @property
+    def collection(self: str):
+        """ Returns `users` collection """
+        return self.client.collection('users')
+
+    def get(self, user: str):
+        """ Gets user data """
+        document = self.collection.document(user)
+        snapshot = document.get()
+        return snapshot.to_dict()
+
+    def set(self, user: str, data: dict):
+        """ Sets user data """
+        document = self.collection.document(user)
+        return document.set(data)

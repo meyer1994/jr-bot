9bd4a29bbf24b72468564388290d3dcfe6311d62
Joao Meyer <1994meyer@gmail.com>
2020-05-10 00:27:05 -0300

    TDD FTW

---
+++ tests/test_transcripts.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from unittest.mock import patch, PropertyMock
+
+from audiobot.transcripts import Transcripts
+
+
+@patch('audiobot.transcripts.SearchClient')
+class TestTranscripts(TestCase):
+    def test_constructor(self, algolia):
+        """ Creates an algolia client on constructor """
+        Transcripts()
+        algolia.Create.assert_called_once_with('user', 'token')
+
+    def test_index(self, algolia):
+        """ Returns `audio` index """
+        transcripts = Transcripts()
+        transcripts.index
+        algolia.Create().init_index.assert_called_once_with('audios')
+
+    @patch.object(Transcripts, 'index', new_callable=PropertyMock)
+    def test_save(self, index, algolia):
+        """ Saves an item to the index """
+        transcripts = Transcripts()
+        data = {'some': 'data'}
+        transcripts.save('user', data)
+        data = {'user': 'user', 'some': 'data'}
+        config = {'autoGenerateObjectIDIfNotExist': True}
+        index().save_object.assert_called_once_with(data, config)
+
+    @patch.object(Transcripts, 'index', new_callable=PropertyMock)
+    def test_search(self, index, algolia):
+        """ Searches an item in the index """
+        transcripts = Transcripts()
+        transcripts.search('user', 'text')
+        options = {
+            'attributesToHighlight': [],
+            'filters': 'user = user',
+            'hitsPerPage': 1
+        }
+        index().search.assert_called_once_with('text', options)

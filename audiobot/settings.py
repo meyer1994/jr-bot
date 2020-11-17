67c87c85323b8a4e3756b2697ccaa582bbca1408
Joao Meyer <1994meyer@gmail.com>
2020-11-17 00:03:58 -0300

    Added google settings

--- audiobot/settings.py
+++ audiobot/settings.py
@@ -1,27 +1,17 @@
 from pydantic import BaseSettings
 
 
-class AlgoliaSettings(BaseSettings):
-    user: str = 'user'
-    token: str = 'token'
-    index: str = 'index'
-
-    class Config:
-        env_file = '.env'
-        env_prefix = 'algolia_'
-
-
-class GoogleSettings(BaseSettings):
-    storage_bucket: str = 'bucket'
-
-    class Config:
-        env_prefix = 'google_'
-        env_file = '.env'
-
-
-class TelegramSettings(BaseSettings):
+class Telegram(BaseSettings):
     token: str = 'token'
 
     class Config:
         env_prefix = 'telegram_'
+        env_file = '.env'
+
+
+class Google(BaseSettings):
+    bucket: str = 'bucket'
+
+    class Config:
+        env_prefix = 'google_'
         env_file = '.env'

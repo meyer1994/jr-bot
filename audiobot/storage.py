980f58e2546d322691adc3a091c70aabe11c16f2
Joao Meyer <1994meyer@gmail.com>
2020-11-18 00:28:47 -0300

    Added storage tests

--- audiobot/storage.py
+++ audiobot/storage.py
@@ -9,28 +9,25 @@ from audiobot import settings
 GOOGLE = settings.Google()
 
 
+def _blob(key: str) -> object:
+    return storage.Client()\
+        .bucket(GOOGLE.bucket)\
+        .blob(key)
+
+
 def download(key: str) -> IO:
     data = io.BytesIO()
-    storage.Client()\
-        .bucket(GOOGLE.bucket)\
-        .blob(key)\
-        .download_to_file(data)
+    _blob(key).download_to_file(data)
     data.seek(0)
     return data
 
 
 def upload(data: IO, key: str) -> str:
     data.seek(0)
-    storage.Client()\
-        .bucket(GOOGLE.bucket)\
-        .blob(key)\
-        .upload_from_file(data)
+    _blob(key).upload_from_file(data)
     data.seek(0)
     return f'gs://{GOOGLE.bucket}/{key}'
 
 
 def exists(key: str) -> bool:
-    return storage.Client()\
-        .bucket(GOOGLE.bucket)\
-        .blob(key)\
-        .exists()
+    return _blob(key).exists()
---
+++ tests/test_storage.py
@@ -0,0 +1,35 @@
+import io
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from audiobot import storage
+
+
+class TestAudio(TestCase):
+    @patch('audiobot.storage._blob')
+    def test_download(self, _blob):
+        """ Calls GCP client to download blob """
+        data = storage.download('key')
+
+        _blob.assert_called_once_with('key')
+        _blob().download_to_file.assert_called_once()
+
+    @patch('audiobot.storage._blob')
+    def test_upload(self, _blob):
+        """ Calls GCP client to upload blob """
+        data = io.BytesIO(b'data')
+
+        uri = storage.upload(data, 'key')
+
+        self.assertEqual(uri, 'gs://bucket/key')
+
+        _blob.assert_called_once_with('key')
+        _blob().upload_from_file.assert_called_once_with(data)
+
+    @patch('audiobot.storage._blob')
+    def test_exists(self, _blob):
+        """ Calls GCP client to check if blob exists """
+        storage.exists('key')
+
+        _blob.assert_called_once_with('key')
+        _blob().exists.assert_called_once_with()

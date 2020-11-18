67b6463ad06ccbb32d6c0b4311b94f83d56c4e15
Joao Meyer <1994meyer@gmail.com>
2020-11-18 00:21:08 -0300

    Added index tests

--- audiobot/index.py
+++ audiobot/index.py
@@ -5,21 +5,18 @@ from audiobot import settings
 ALGOLIA = settings.Algolia()
 
 
-def index(uri: str, text: str) -> str:
+def _index() -> object:
     client = SearchClient.create(ALGOLIA.user, ALGOLIA.token)
-    index = client.init_index(ALGOLIA.index)
+    return client.init_index(ALGOLIA.index)
 
-    data = {'objectID': key, 'text': transcription}
-    result = index.save_object(data, config)
 
-    return uri
+def save(uri: str, text: str) -> str:
+    index = _index()
+    data = {'objectID': uri, 'text': text}
+    return index.save_object(data)
 
 
 def search(text: str) -> object:
-    client = SearchClient.create(ALGOLIA.user, ALGOLIA.token)
-    index = client.init_index(ALGOLIA.index)
-
-    config = {'hitPerPage': 1}
-    search = index.search(text, config)
-
-    return search
+    index = _index()
+    htis = index.search(text)
+    return htis['hits']
---
+++ tests/test_index.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from audiobot import index
+
+
+class TestIndex(TestCase):
+    @patch('audiobot.index._index')
+    def test_save(self, _index):
+        """ Calls Algolia client to save entry """
+        response = {'objectID': 'uri'}
+        _index().save_object.return_value = response
+
+        saved = index.save('uri', 'text')
+        self.assertDictEqual(saved, response)
+
+        payload = {'objectID': 'uri', 'text': 'text'}
+        _index().save_object.assert_called_once_with(payload)
+
+    @patch('audiobot.index._index')
+    def test_search(self, _index):
+        """ Calls Algolia client to search for entry """
+        response = {'hits': [{'objectID':' uri', 'text': 'text'}]}
+        _index().search.return_value = response
+
+        searched = index.search('text')
+        self.assertListEqual(searched, response['hits'])

28f20a4fbec96ab59cf5856b7022009fe66bcdcb
Joao Meyer <1994meyer@gmail.com>
2020-11-18 22:42:52 -0300

    Added log

--- audiobot/storage.py
+++ audiobot/storage.py
@@ -1,4 +1,5 @@
 import io
+import logging
 from typing import IO
 
 from google.cloud import storage
@@ -6,6 +7,9 @@ from google.cloud import storage
 from audiobot import settings
 
 GOOGLE = settings.Google()
+
+logger = logging.getLogger('storage')
+logger.setLevel(logging.INFO)
 
 
 def _blob(key: str) -> object:
@@ -15,18 +19,25 @@ def _blob(key: str) -> object:
 
 
 def download(key: str) -> IO:
+    logger.info('Downloading: %s', key)
     data = io.BytesIO()
     _blob(key).download_to_file(data)
     data.seek(0)
+    logger.info('Downloaded: %s', key)
     return data
 
 
 def upload(data: IO, key: str) -> str:
+    logger.info('Uploading: %s', key)
     data.seek(0)
     _blob(key).upload_from_file(data)
     data.seek(0)
+    logger.info('Uploaded: %s', key)
     return f'gs://{GOOGLE.bucket}/{key}'
 
 
 def exists(key: str) -> bool:
-    return _blob(key).exists()
+    logger.info('Checking for: %s', key)
+    result = _blob(key).exists()
+    logger.info('Checking for: %s', key)
+    return result
